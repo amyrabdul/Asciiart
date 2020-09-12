0c006d12168664eabf9599e53e9766080b04c8dd
amyrabdul <amyrabdul@hotmail.com>
2020-09-11 23:13:26 -0300

    exercicio asciiart

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Asciiart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ||  ||");    
+            Console.WriteLine(" \\\\()//");  
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+        
+            Console.ReadKey();
+        }
+    }
+}
